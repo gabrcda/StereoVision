c4cd872b09db465efbfa8b211857a8ad25df1571
Gabriel Cardoso de Avelar <gabri22kocebrac@gmail.com>
2023-01-26 17:59:03 -0300

    Delete visionstereo.py

--- VisionTest/visionstereo.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import cv2 as cv
-from matplotlib import pyplot as plt
-imgL = cv.imread('scene1.row3.col1.ppm', cv.IMREAD_GRAYSCALE)
-imgR = cv.imread('scene1.row3.col2.ppm', cv.IMREAD_GRAYSCALE)
-stereo = cv.StereoBM_create(numDisparities=16, blockSize=15)
-disparity = stereo.compute(imgL,imgR)
-plt.imshow(disparity, 'gray')
-plt.show()
