02aedc59dbfe83fabd14736c77a7bf3be86829dc
Gabriel Cardoso de Avelar <gabri22kocebrac@gmail.com>
2023-02-06 00:23:30 -0300

    Update main.py

--- VisionTest/main.py
+++ VisionTest/main.py
@@ -3,7 +3,6 @@ from stereo_cam import StereoCam
 camera = StereoCam()
 camera.settings_cam_running()
 camera.capFrame()
-#camera.getImagesCalibrateCam()
 #pre_processing = PreProcessing()
 #pre_processing.crop_images()
 #pre_processing.plot_diparity()
